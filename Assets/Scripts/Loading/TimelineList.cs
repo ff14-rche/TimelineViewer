3891214321ea9c601d1fc98dadb5f1aebb635dae
喜多 誠 <kita@kitanoMacBook-Pro.local>
2017-09-12 19:33:24 +0900

    プロジェクトの追加

---
+++ Assets/Scripts/Loading/TimelineList.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimelineList : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _TimelineObjPrefab = null;
+    [SerializeField] private ScrollRect _TimelineListView = null;
+
+    void Awake()
+    {
+        for (int i = 0; i < 10; ++i)
+        {
+
+            var obj = Instantiate(_TimelineObjPrefab);
+
+            obj.transform.SetParent(_TimelineListView.content);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Utility/TimelineTextReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineTextReader : MonoBehaviour
+{
+
+    void Awake()
+    {
+        var files = Directory.GetFiles(Application.dataPath + "/Resources/Timeline/", "*.txt");
+        files.ToList().ForEach(c =>
+        {
+            Debug.Log(c);
+        });
+
+        StreamReader sr = new StreamReader(Application.dataPath + "/Resources/Timeline/omega_delta_04_2.txt");
+        try
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (!line.StartsWith("#"))
+                {
+                    Debug.Log(line);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+        sr.Close();
+    }
+}
